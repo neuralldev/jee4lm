c2f21d9d012bce99db1565b2838a430bdd52b5af
neuralldev <thierry.gluzman@neurall.fr>
2024-09-02 17:08:34 +0200

    ccc

--- core/class/jee4lm.class.php
+++ core/class/jee4lm.class.php
@@ -1082,8 +1082,8 @@ public function startBackflush()
       'template' => 'tmplicon',
       'display' => array('icon' => 'null'),
       'replace' => array(
-        '#_icon_on_#' => "<img class='img-responsive' src='/plugins/jee4lm/ressources/main_on.png' width='64' height='64'>",
-        '#_icon_off_#' => "<img class='img-responsive' src='/plugins/jee4lm/ressources/main_on.png' width='64' height='64'>",
+        '#_icon_on_#' => "<img class='img-responsive' src='/plugins/jee4lm/core/config/main_on.png' width='64' height='64'>",
+        '#_icon_off_#' => "<img class='img-responsive' src='/plugins/jee4lm/core/config/main_on.png' width='64' height='64'>",
         "#_time_widget_#" =>"0"
         )
     );
