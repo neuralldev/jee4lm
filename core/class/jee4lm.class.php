6e3bf6a61310042bc13e47cd4eb9547c3a6d7fd8
neuralldev <thierry.gluzman@neurall.fr>
2024-09-29 17:34:47 +0200

    cc

--- core/class/jee4lm.class.php
+++ core/class/jee4lm.class.php
@@ -252,7 +252,7 @@ class jee4lm extends eqLogic
           $slug = $jee4lm->getConfiguration('type');
           $id = $jee4lm->getId();
           $state = 0 + cmd::byEqLogicIdAndLogicalId($id, 'machinemode')->execCmd();
-          log::add(__CLASS__, 'debug', "cron ID=$id serial=$serial slug=$slug");
+          log::add(__CLASS__, 'debug', "cron ID=$id serial=$serial slug=$slug state=".$state);
           if ($slug != '') {
             $token = self::getToken(); // send query for token and refresh it if necessary
             if ($token != '')
