8edfd95c955c34553b415969e0ffc41bc6f0bcf1
neuralldev <thierry.gluzman@neurall.fr>
2024-10-26 18:12:20 +0200

    j

--- core/class/mDNS.php
+++ core/class/mDNS.php
@@ -2,6 +2,12 @@
 
 require_once dirname(__FILE__) . '/../../../../core/php/core.inc.php';
 
+include_file('core', 'authentification', 'php');
+
+if (!isConnect('admin')) {
+	throw new Exception(__('401 - {{Accès non autorisé}}', __FILE__));
+}
+
 // Simple MDNS query object
 // Chris Ridings
 // www.chrisridings.com
