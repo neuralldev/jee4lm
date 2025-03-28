6e81103d317648a580fe5370e10e6e3c7b255263
neuralldev <thierry.gluzman@neurall.fr>
2025-03-28 20:04:31 +0100

    ddd

--- core/class/jee4lm5.class.php
+++ core/class/jee4lm5.class.php
@@ -1190,12 +1190,17 @@ public function setScaleTarget($_dose, $_weight) {
             log::add(__CLASS__, 'debug', 'getinformation machine status=' . $w['output']['mode']);
             $this->checkAndUpdateCmd('machinestatus',$w['output']['mode'] == 'BrewingMode');
             $cmd = $this->getCmd(null, 'jee4lm_on');
-            $cmd->setIsVisible($w['output']['mode'] == 'BrewingMode'?0:1);
-            $cmd->save();
-            $this->checkAndUpdateCmd('machinestatus',$w['output']['mode'] == 'BrewingMode');
-            $cmd = $this->getCmd(null, 'jee4lm_off');
-            $cmd->setIsVisible($w['output']['mode'] == 'BrewingMode'?1:0);
-            $cmd->save();
+            if ($cmd != null) {
+              $cmd->setIsVisible($w['output']['mode'] == 'BrewingMode'?1:0);
+              $cmd->setIsVisible($w['output']['mode'] == 'BrewingMode'?0:1);
+              $cmd->save();
+            }
+            $this->checkAndUpdateCmd('machinemode',$w['output']['mode'] == 'BrewingMode');
+            if ($cmd != null) {
+              $cmd = $this->getCmd(null, 'jee4lm_off');
+              $cmd->setIsVisible($w['output']['mode'] == 'BrewingMode'?1:0);
+              $cmd->save();
+            }
             break;
           case "CMCoffeeBoiler":
             log::add(__CLASS__, 'debug', 'getinformation coffee boiler temp=' . $w['output']['temperature']. " starts in ". $w['output']['readyStartTime']);
