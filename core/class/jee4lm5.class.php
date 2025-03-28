27bfd409f86e0c5fae8b2fb9be14efabbe48900a
neuralldev <thierry.gluzman@neurall.fr>
2025-03-28 06:55:00 +0100

    po

--- core/class/jee4lm5.class.php
+++ core/class/jee4lm5.class.php
@@ -250,7 +250,8 @@ class jee4lm5 extends eqLogic
         /* lire les infos de l'équipement ici */
         $slug = $jee4lm->getConfiguration('type');
         $id = $jee4lm->getId();
-        $state = 0 + cmd::byEqLogicIdAndLogicalId($id, 'machinemode')->execCmd();
+        $m = cmd::byEqLogicIdAndLogicalId($id, 'machinemode');
+        $state = 0 + $m->execCmd();
         log::add(__CLASS__, 'debug', "cron ID=$id serial=$serial slug=$slug state=$state");
         if ($slug != '') { // if there is a type of machine defined 
           if ($state == 0) { // if machine is off, refresh information only every 5 minutes if on the web, every minute if local
