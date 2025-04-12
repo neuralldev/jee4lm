c80bb750f43bebffde7f47fcdda15cb926315f7e
neuralldev <thierry.gluzman@neurall.fr>
2025-04-12 07:59:19 +0200

    bbw dose

--- core/class/jee4lm5.class.php
+++ core/class/jee4lm5.class.php
@@ -410,10 +410,10 @@ class jee4lm5 extends eqLogic
           $_eq->AddCommand("Continu", 'bbwfree', 'info', 'binary', PLUGINNAME."::bbw nodose", null, null, 0, 'default', 'default', 'default', 'default', null, 0, false, null, null, null, 0);
           $_eq->AddCommand("Dose 1", 'bbwdoseA', 'info', 'numeric', PLUGINNAME."::bbw dose inactive", "g",0);
           $_eq->AddCommand("Dose 2", 'bbwdoseB', 'info', 'numeric', PLUGINNAME."::bbw dose inactive", "g",0);
-          $_eq->AddAction("jee4lm_bbwA", "BBW Dose A", "button", "", 0);
-          $_eq->AddAction("jee4lm_bbwB", "BBW Dose B", "button", "", 0);
-          $_eq->AddAction("jee4lm_doseA_slider", "Régler Dose A", "button", "", 1, "slider", 5, 100, 0.5);
-          $_eq->AddAction("jee4lm_doseB_slider", "Régler Dose B", "button", "", 1, "slider", 5, 100, 0.5);
+          $_eq->AddAction("jee4lm_bbwA", "BBW Dose 1", "button", "", 0);
+          $_eq->AddAction("jee4lm_bbwB", "BBW Dose 2", "button", "", 0);
+          $_eq->AddAction("jee4lm_doseA_slider", "Régler Dose 1", "button", "", 1, "slider", 5, 100, 0.5);
+          $_eq->AddAction("jee4lm_doseB_slider", "Régler Dose 2", "button", "", 1, "slider", 5, 100, 0.5);
           $_eq->linksetpoint("jee4lm_doseA_slider", "bbwdoseA");
           $_eq->linksetpoint("jee4lm_doseB_slider", "bbwdoseB");
         }
